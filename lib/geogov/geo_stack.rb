cfdc6bdeebaad4cefbee06b4e0c54afb71570563
Gareth Rushgrove <gareth@morethanseven.net>
2011-12-22 13:36:42 +0000

    put this back as we found it

--- lib/geogov/geo_stack.rb
+++ lib/geogov/geo_stack.rb
@@ -16,7 +16,7 @@ module Geogov
       if self.lat and self.lon
         return FuzzyPoint.new(self.lat, self.lon, :point)
       end
-     
+
       if self.postcode
         district = postcode.split(" ")[0]
         district_centre = Geogov.centre_of_district(district)
@@ -36,14 +36,13 @@ module Geogov
     end
 
     def self.new_from_ip(ip_address)
-      #remote_location = Geogov.remote_location(ip_address)
-      new()
-      # do |gs|
-      #  if remote_location
-      #    gs.country = remote_location['country']
-      #  end
-      #  gs.fuzzy_point = gs.calculate_fuzzy_point
-      # end
+      remote_location = Geogov.remote_location(ip_address)
+      new() do |gs|
+        if remote_location
+          gs.country = remote_location['country']
+        end
+        gs.fuzzy_point = gs.calculate_fuzzy_point
+      end
     end
 
     def self.new_from_hash(hash)
